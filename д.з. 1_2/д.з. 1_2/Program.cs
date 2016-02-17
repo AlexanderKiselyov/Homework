1a01d1f3dedd93dd1846737f5b377868be63f974
AlexanderKiselyov <a_kiselyov-snz@mail.ru>
2016-02-17 13:50:51 +0300

    1_2

---
+++ д.з. 1_2/д.з. 1_2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace д.з._1_2
+{
+    class Program
+    {
+        static int fibonacci(int n)
+        {
+            if ((n == 1) || (n == 2))
+            {
+                return 1;
+            }
+            else
+            {
+                return (fibonacci(n - 1) + fibonacci(n - 2));
+            }
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Write number: ");
+            int n = int.Parse(Console.ReadLine());
+            Console.WriteLine("The result is: ");
+            Console.WriteLine(fibonacci(n));
+        }
+    }
+}
+
