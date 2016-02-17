1fa933f3413d4f7c993cb17c199be3c8ca070d02
AlexanderKiselyov <a_kiselyov-snz@mail.ru>
2016-02-17 22:25:13 +0300

    1_1

---
+++ д.з. 1_1/д.з. 1_1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace д.з._1_1
+{
+    class Program
+    {
+	static int factorial(int result, int n)
+	{
+        for (int i = 1; i <= n; i++)
+        {
+            result *= i;
+        }
+		return result;
+	}
+	static void Main(string[] args)
+        {
+			Console.WriteLine ("Write number: ");
+			int n = int.Parse (Console.ReadLine ());
+			int result = 1;
+			result = factorial(result, n);
+            Console.WriteLine("The result is: ");
+            Console.WriteLine(result);
+        }
+    }
+}
