804c5bbe112f7d170bf8742aba0080e5c617be60
Alexander <a_kiselyov-snz@mail.ru>
2016-02-20 21:38:36 +0300

    Update Program.cs

--- д.з. 1_5/д.з. 1_5/Program.cs
+++ д.з. 1_5/д.з. 1_5/Program.cs
@@ -4,41 +4,46 @@ namespace д.з._1_5
 {
 	class Program
 	{
-		static void Main (string[] args)
+		static void MatrixBubbleSort(int[,] array, int m, int n)
 		{
-			Console.WriteLine ("Write number of string and columns:");
-			int n = int.Parse (Console.ReadLine ());
-			int m = int.Parse (Console.ReadLine ());
-			int[,] array = new int[n , m];
-			Console.WriteLine ("Enter array of " + n * m + " elements:");
-			for (int i = 0; i < n; i++)
-			{
-				for (int j = 0; j < m; j++)
-				{
-					array[i,j] = int.Parse (Console.ReadLine ());
-				}
-			}
 			for (int i = 0; i < m; i++)
 			{
 				for (int j = m - 1; j > i; j--)
 				{
-					if (array[0 , j - 1] > array[0 , j])
+					if (array[0, j - 1] > array[0, j])
 					{
 						for (int k = 0; k < n; k++)
 						{
-							int current = array[k , j - 1];
-							array[k , j - 1] = array[k , j];
-							array[k , j] = current;
+							int current = array[k, j - 1];
+							array[k, j - 1] = array[k, j];
+							array[k, j] = current;
 						}
 					}
 				}
 			}
-			Console.WriteLine ("New array:");
+		}
+		
+		static void Main (string[] args)
+		{
+			Console.WriteLine("Write number of string and columns:");
+			int n = int.Parse(Console.ReadLine ());
+			int m = int.Parse(Console.ReadLine ());
+			int[,] array = new int[n , m];
+			Console.WriteLine("Enter array of " + n * m + " elements:");
 			for (int i = 0; i < n; i++)
 			{
 				for (int j = 0; j < m; j++)
 				{
-					Console.WriteLine (array[i , j]);
+					array[i, j] = int.Parse(Console.ReadLine ());
+				}
+			}
+			MatrixBubbleSort(array, m, n);
+			Console.WriteLine("New array:");
+			for (int i = 0; i < n; i++)
+			{
+				for (int j = 0; j < m; j++)
+				{
+					Console.WriteLine(array[i, j]);
 				}
 			}
 		}
