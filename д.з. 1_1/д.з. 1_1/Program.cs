beb1e5552540d38b12554b8ab419a146d43297b4
Alexander <a_kiselyov-snz@mail.ru>
2016-02-20 21:03:25 +0300

    Update Program.cs

--- д.з. 1_1/д.з. 1_1/Program.cs
+++ д.з. 1_1/д.з. 1_1/Program.cs
@@ -4,22 +4,23 @@ namespace д.з._1_1
 {
     class Program
     {
-	static int factorial(int result, int n)
+	static int Factorial(int result, int n)
 	{
-        for (int i = 1; i <= n; i++)
-        {
-            result *= i;
-        }
+        	for (int i = 1; i <= n; i++)
+        	{
+            		result *= i;
+        	}
 		return result;
 	}
+	
 	static void Main(string[] args)
         {
-			Console.WriteLine ("Write number: ");
-			int n = int.Parse (Console.ReadLine ());
-			int result = 1;
-			result = factorial(result, n);
-            Console.WriteLine("The result is: ");
-            Console.WriteLine(result);
+        	Console.WriteLine ("Write number: ");
+		int n = int.Parse (Console.ReadLine ());
+		int result = 1;
+		result = Factorial(result, n);
+		Console.WriteLine("The result is: ");
+        	Console.WriteLine(result);
         }
     }
 }
