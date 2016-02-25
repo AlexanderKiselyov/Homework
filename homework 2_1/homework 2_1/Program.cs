0bd7cdf9314329e1a419b64acc90dd38dd8b5f78
AlexanderKiselyov <a_kiselyov-snz@mail.ru>
2016-02-26 00:09:37 +0300

    2_1

---
+++ homework 2_1/homework 2_1/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Stack
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Stack stack = new Stack();
+			stack.Push(10);
+			stack.Push(7);
+			stack.Push(2);
+			Console.WriteLine(stack.Pop());
+			Console.WriteLine(stack.Pop());
+			Console.WriteLine(stack.Pop());
+		}
+	}
+
+	class Stack
+	{
+		private	List<int> list = new List<int>();
+
+		public void Push(int number) // adds elements to the stack
+		{
+			list.Add(number);
+		}
+
+		public int Pop() // takes elements off the stack and shows them
+		{
+			if (list.Count == 0)
+			{
+				return 0;
+			}
+			int result = list[list.Count - 1];
+			list.RemoveAt(list.Count - 1);
+			return result;
+		}
+	}
+}
