22dc57e91f2a7d9d9df97270ae5483ffabe9c428
AlexanderKiselyov <a_kiselyov-snz@mail.ru>
2016-03-11 16:35:55 +0300

    2_4

---
+++ homework 2_4/homework 2_4/Calculator.cs
@@ -0,0 +1,58 @@
+﻿namespace Calculator
+{
+	/// Adds, Subtracts, Divides and Multiplies two elements.
+	public class Calculator
+	{
+		/// creats Calculator object
+		public Calculator(Interface stack)
+		{
+			this.stack = stack;
+		}
+
+		private Interface stack;
+
+		/// pushes element to stack
+		public void Push(int value)
+		{
+			stack.Push(value);
+		}
+
+		/// adds two elements
+		public void Add()
+		{
+			int first = stack.Pop();
+			int second = stack.Pop();
+			stack.Push(second + first);
+		}
+
+		/// subtracts two elements
+		public void Subtract()
+		{
+			int first = stack.Pop();
+			int second = stack.Pop();
+			stack.Push(second - first);
+		}
+
+		/// multiplies two elements
+		public void Multiply()
+		{
+			int first = stack.Pop();
+			int second = stack.Pop();
+			stack.Push(second * first);
+		}
+
+		/// divides two elements
+		public void Divide()
+		{
+			int first = stack.Pop();
+			int second = stack.Pop();
+			stack.Push(second / first);
+		}
+
+		/// returns the result to the stack
+		public int Result()
+		{
+			return stack.Top();
+		}
+	}
+}
---
+++ homework 2_4/homework 2_4/List.cs
@@ -0,0 +1,76 @@
+﻿using System;
+
+namespace Calculator
+{
+	public class List : Interface
+	{
+		private ListElement list = new ListElement();
+
+		/// adds elements to the list
+		public void Push(int value)
+		{
+			list.Add(value);
+		}
+
+		/// takes elements off the list
+		public void DeleteElement(int value)
+		{
+			list.SetIteratorFirst();
+			while (!list.IfIteratorNull() && list.ReturnValue() != value)
+			{
+				list.MoveIterator();
+			}
+			if (!list.IfIteratorNull())
+			{
+				list.DeleteElement();
+			}
+		}
+
+		/// shows all the elements from the list
+		public void Show()
+		{
+			list.SetIteratorFirst();
+			while (!list.IfIteratorNull())
+			{
+				Console.Write(list.ReturnValue());
+				Console.Write(' ');
+				list.MoveIterator();
+			}
+			Console.WriteLine();
+			list.SetIteratorFirst();
+		}
+
+		///  returns the first element and deletes it
+		public int Pop()
+		{
+			list.SetIteratorLast();
+			int value = list.ReturnValue();
+			list.DeleteElement();
+			return value;
+		}
+
+		/// returns value of the last element
+		public int Top()
+		{
+			list.SetIteratorLast();
+			return list.ReturnValue();
+		}
+
+		/// checks if the elements is in the list
+		public bool Search(int value)
+		{
+			list.SetIteratorFirst();
+			while (!list.IfIteratorNull())
+			{
+				if (list.ReturnValue() == value)
+				{
+					list.SetIteratorFirst();
+					return true;
+				}
+				list.MoveIterator();
+			}
+			list.SetIteratorFirst();
+			return false;
+		}
+	}
+}
