4b6d545964c364b1a8134d2e3ea8e9d1f10d59c9
AlexanderKiselyov <a_kiselyov-snz@mail.ru>
2016-03-11 16:43:22 +0300

    3_1

---
+++ homework 3_1/StackTest/StackTest.cs
@@ -0,0 +1,47 @@
+﻿namespace MyStack.Test
+{
+	using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+	[TestClass]
+	public class StackTest
+	{
+		private Stack stack;
+
+		[TestInitialize]
+		public void Initialize()
+		{
+			stack = new Stack();
+		}
+
+		[TestMethod]
+		public void PushTest()
+		{
+			stack.Push(1);
+			Assert.IsFalse(stack.IsEmpty());
+		}
+
+		[TestMethod]
+		public void PopTest()
+		{
+			stack.Push(1);
+			Assert.AreEqual(1, stack.Pop());
+		}
+
+		[TestMethod]
+		public void TwoElementsPopTest()
+		{
+			stack.Push(1);
+			stack.Push(2);
+			Assert.AreEqual(2, stack.Pop());
+			Assert.AreEqual(1, stack.Pop());
+		}
+
+		[TestMethod]
+		[ExpectedException(typeof(StackNullException))]
+		public void PopFromEmptyStackTest()
+		{
+			stack.Pop();
+		}
+	}
+}
+
