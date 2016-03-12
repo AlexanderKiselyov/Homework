f5f7f849d7cb2f87a0fbb789d9a90454dfb776a3
AlexanderKiselyov <a_kiselyov-snz@mail.ru>
2016-03-12 15:44:40 +0300

    2_3

---
+++ homework 2_3/HashTableTest/HashTableTest.cs
@@ -0,0 +1,29 @@
+﻿namespace NewHashTable.Test
+{
+	using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+	[TestClass]
+	public class HashTableTest
+	{
+		private HashTable tmp;
+
+		[TestInitialize]
+		public void Inizialize()
+		{
+			tmp = new HashTable();
+		}
+
+		[TestMethod]
+		public void HashTableTests()
+		{
+			tmp.Add(100);
+			tmp.Add(56);
+			tmp.Add(87);
+			Assert.AreEqual(true, tmp.Search(87));
+			tmp.Add(96);
+			tmp.DeleteElement(96);
+			Assert.AreEqual(false, tmp.Search(96));
+			tmp.Add(11);
+		}
+	}
+}
---
+++ homework 2_3/homework 2_3/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace NewHashTable
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			HashTable hash = new HashTable();
+			hash.Add(10);
+			hash.Add(95);
+			hash.Add(80);
+			hash.Add(1);
+			hash.Show();
+			hash.DeleteElement(95);
+			hash.DeleteElement(5);
+			hash.Show();
+			Console.WriteLine(hash.Search(1));
+			Console.WriteLine(hash.Search(2));
+			hash.DeleteElement(80);
+			hash.Show();
+			hash.DeleteElement(10);
+			hash.DeleteElement(1);
+			hash.DeleteElement(78);
+			hash.Show();
+			Console.WriteLine(hash.Search(95));
+		}
+	}
+}
