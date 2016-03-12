dad32d1b78fa26102b9e177da7d88d580711b317
AlexanderKiselyov <a_kiselyov-snz@mail.ru>
2016-03-12 16:09:12 +0300

    2_4

---
+++ homework 2_4/homework 2_4/Interface.cs
@@ -0,0 +1,15 @@
+﻿namespace Calculator
+{
+	/// Interface with "Push", "Pop" and "Top" methods.
+	public interface CalculatorInterface
+	{
+		/// adds value to the Stack
+		void Push(int value);
+
+		/// returns last element and deletes it
+		int Pop();
+
+		/// returns top element
+		int Top();
+	}
+}
