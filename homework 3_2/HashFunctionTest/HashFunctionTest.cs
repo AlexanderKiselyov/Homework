b5cc8622e31cf974e7010b9b71f213c28e7563f6
AlexanderKiselyov <a_kiselyov-snz@mail.ru>
2016-03-14 00:40:03 +0300

    3_2

---
+++ homework 3_2/HashFunctionTest/HashFunctionTest.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace NewHashTable.Tests
+{
+	[TestClass]
+	public class HashTableTests
+	{
+		private HashTable hashTable;
+
+		[TestInitialize]
+		public void Initialization()
+		{
+			hashTable = new HashTable(new FirstHash()); // you can change: "new HashTable(new SecondHash());"
+		}
+
+		[TestMethod]
+		public void AddAndSearch()
+		{
+			hashTable.Add(1);
+			hashTable.Add(2);
+			Assert.AreEqual(true, hashTable.Search(1));
+			Assert.AreEqual(true, hashTable.Search(2));
+			Assert.AreEqual(false, hashTable.Search(3));
+		}
+
+		[TestMethod]
+		public void DeleteElement()
+		{
+			hashTable.DeleteElement(1);
+			hashTable.Add(1);
+			hashTable.DeleteElement(1);
+			Assert.AreEqual(false, hashTable.Search(1));
+		}
+
+		[TestMethod]
+		[ExpectedException(typeof(ListNullException))]
+		public void StandardException()
+		{
+			ArrayList stack = new ArrayList();
+			stack.ReturnValue();
+		}
+	}
+}
---
+++ homework 3_2/homework 3_2/HashTable.cs
@@ -0,0 +1,53 @@
+﻿namespace NewHashTable
+{
+	/// counts hash function for the element and add it to the array. Also can delete elements from the hash table, search them in it and show hash table.
+	public class HashTable
+	{
+		private HashInterface hash;
+		private const int hashConstant = 100;
+		private List[] list;
+
+		public HashTable(HashInterface hash)
+		{
+			this.hash = hash;
+			list = new List[hashConstant];
+			for (int i = 0; i < hashConstant; ++i)
+			{
+				list[i] = new List();
+			}
+		}
+
+		/// counts hash function
+		private int HashFunction(int value)
+		{
+			return hash.Function(value, hashConstant);
+		}
+
+		/// adds elements with their hash to the table
+		public void Add(int value)
+		{
+			list[HashFunction(value)].Add(value);
+		}
+
+		/// deletes elements with their hash from the table
+		public void DeleteElement(int value)
+		{
+			list[HashFunction(value)].DeleteElement(value);
+		}
+
+		// searchers for the elements in the table
+		public bool Search(int value)
+		{
+			return list[HashFunction(value)].Search(value);
+		}
+
+		/// shows elements
+		public void Print()
+		{
+			for (int i = 0; i < hashConstant; ++i)
+			{
+				list[i].Print();
+			}
+		}
+	}
+}
