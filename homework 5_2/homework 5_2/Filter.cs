5c386011466889dfbaaac6fcc60be5d3f85daba9
AlexanderKiselyov <a_kiselyov-snz@mail.ru>
2016-03-19 23:46:56 +0300

    5_2

---
+++ homework 5_2/homework 5_2/Filter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Filter
+{
+	/// class that can check if the element with the quality from the function is in the list
+	public static class FilterFunction
+	{
+		/// returns elements with true meaning from the function
+		static public List<int> Filter(List<int> list, Func<int, bool> function)
+		{
+			List<int> result = new List<int>();
+			for (int i = 0; i < list.Count; i++)
+			{
+				if (function(list[i]))
+				{
+					result.Add(list[i]);
+				}
+			}
+			return result;
+		}
+	}
+}
---
+++ homework 5_2/homework 5_2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Filter
+{
+	class Program
+	{
+		static void Main()
+		{
+			List<int> list = FilterFunction.Filter(new List<int>() { 1, 2, 3 }, x => { return x % 2 == 0; });
+			Console.WriteLine("New elements:");
+			foreach (int element in list)
+			{
+				Console.Write("{0} ", element);
+			}
+			Console.WriteLine();
+		}
+	}
+}
