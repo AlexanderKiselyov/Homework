099876cd176e680ff4bf23cceb721713bf45fb4e
AlexanderKiselyov <a_kiselyov-snz@mail.ru>
2016-03-19 23:21:29 +0300

    5_1

---
+++ homework 5_1/MapTests/MapTests.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MapFunction.Test
+{
+	[TestClass]
+	public class MapTests
+	{
+		[TestMethod]
+		public void MapTest1()
+		{
+			Assert.AreEqual(MapFunction.Map(new List<int>() { 1, 2, 3 }, x => x * 2), new List<int>() { 2, 4, 6 });
+		}
+
+		[TestMethod]
+		public void MapTest2()
+		{
+			Assert.AreEqual(MapFunction.Map(new List<int>() { 1, 2, 3 }, x => x * x * x), new List<int>() { 1, 8, 27 });
+		}
+
+		[TestMethod]
+		public void MapTest3()
+		{
+			Assert.AreEqual(MapFunction.Map(new List<int>() { 1, 2, 3 }, x => x * 0), new List<int>() { 0, 0, 0 });
+		}
+	}
+}
---
+++ homework 5_1/homework 5_1/MapFunction.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MapFunction
+{
+	/// class that can change input elements with the help of the function
+	public class MapFunction
+	{
+		/// takes an element and returns the result of the function from it
+		static public List<int> Map(List<int> list, Func<int, int> function)
+		{
+			List<int> result = new List<int>();
+			for (int i = 0; i < list.Count; i++)
+			{
+				result.Add(function(list[i]));
+			}
+			return result;
+		}
+	}
+}
