c18a57148ddea03d10b1bd2f722376f45a868d4c
AlexanderKiselyov <a_kiselyov-snz@mail.ru>
2016-03-24 22:46:45 +0300

    5_4

---
+++ homework 5_4/homework 5_4/ConsoleTravel.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace ConsoleTravel
+{
+	/// class that receives signals from EventLoop and move cursor up, down, left or right
+	public class ConsoleTravel
+	{
+		private Coordinates max = new Coordinates();
+		private int x = 79; // length of the travel field
+		private int y = 24; // height of the travel field
+
+		public void Start(object sender, EventArgs args)
+		{
+			Console.CursorTop = 0;
+			for (int i = 0; i < y; i++)
+			{
+				Console.WriteLine();
+			}
+			Console.WriteLine("Type 'C' to close the program.");
+			max = new Coordinates(x, Console.CursorTop - 1);
+			Console.CursorTop -= y + 1;
+		}
+
+		/// moves cursor left, if possible
+		public void Left(object sender, EventArgs args)
+		{
+			try
+			{
+				Console.CursorLeft--;
+			}
+			catch (ArgumentOutOfRangeException)
+			{
+			}
+		}
+
+		/// moves cursor right, if possible
+		public void Right(object sender, EventArgs args)
+		{
+			if (Console.CursorLeft != max.X)
+			{
+				Console.CursorLeft++;
+			}
+		}
+
+		/// moves cursor up, if possible
+		public void Up(object sender, EventArgs args)
+		{
+			if (Console.CursorTop != max.Y - y)
+			{
+				Console.CursorTop--;
+			}
+		}
+
+		/// moves cursor down, if possible
+		public void Down(object sender, EventArgs args)
+		{
+			if (Console.CursorTop != max.Y)
+			{
+				Console.CursorTop++;
+			}
+		}
+	}
+}
---
+++ homework 5_4/homework 5_4/EventLoop.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace ConsoleTravel
+{
+	/// сlass that receives input commands from user and moves cursor or close the program.
+	public class EventLoop
+	{
+		public event EventHandler<EventArgs> Start = (sender, args) => { };
+		public event EventHandler<EventArgs> LeftHandler = (sender, args) => { };
+		public event EventHandler<EventArgs> RightHandler = (sender, args) => { };
+		public event EventHandler<EventArgs> UpHandler = (sender, args) => { };
+		public event EventHandler<EventArgs> DownHandler = (sender, args) => { };
+
+		/// converts Right, Left, Up and Down arrows and 'C' into signals
+		public void Begin()
+		{
+			Start(this, EventArgs.Empty);
+			while (true)
+			{
+				var button = Console.ReadKey(true);
+				switch (button.Key)
+				{
+					case ConsoleKey.LeftArrow:
+						{
+							LeftHandler(this, EventArgs.Empty);
+							break;
+						}
+					case ConsoleKey.RightArrow:
+						{
+							RightHandler(this, EventArgs.Empty);
+							break;
+						}
+					case ConsoleKey.UpArrow:
+						{
+							UpHandler(this, EventArgs.Empty);
+							break;
+						}
+					case ConsoleKey.DownArrow:
+						{
+							DownHandler(this, EventArgs.Empty);
+							break;
+						}
+					case ConsoleKey.C:
+						{
+							return;
+						}
+					default:
+						{
+							break;
+						}
+				}
+			}
+		}
+	}
+}
