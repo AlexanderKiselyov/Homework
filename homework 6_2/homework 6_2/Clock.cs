4e17dcfac0cdb02e93b8d1485c7def4567290c3c
AlexanderKiselyov <a_kiselyov-snz@mail.ru>
2016-03-27 23:34:23 +0300

    6_2

---
+++ homework 6_2/homework 6_2/Clock.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Clock
+{
+	/// <summary>
+	/// shows current date and time
+	/// </summary>
+	public partial class Clock : Form
+	{
+		private TableLayoutPanel field;
+		private Label label;
+
+		/// <summary>
+		/// shows dialogue window and current date and time
+		/// </summary>
+		public Clock()
+		{
+			Size = new Size(SystemInformation.VirtualScreen.Width / 3,
+										   SystemInformation.VirtualScreen.Height / 3); // устанавливает размеры диалогового окна
+			AutoSizeMode = AutoSizeMode.GrowAndShrink; // убирает возможность уменьшать/увеличивать размеры окна
+			label = new Label();
+			label.Text = DateTime.Now.ToString();
+			field = new TableLayoutPanel();
+			field.Controls.Add(label);
+			Controls.Add(field);
+			//BackColor = Color.Green;
+			var dateAndTimer = new Timer();
+			dateAndTimer.Start();
+			dateAndTimer.Tick += ChangeTime;
+		}
+
+		/// <summary>
+		/// changes time every second
+		/// </summary>
+		private void ChangeTime(object sender, EventArgs args)
+		{
+			label.Text = DateTime.Now.ToString();
+		}
+	}
+}
---
+++ homework 6_2/homework 6_2/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Clock
+{
+	public class Program
+	{
+		/// <summary>
+		/// Главная точка входа для приложения.
+		/// </summary>
+		[STAThread]
+		public static void Main()
+		{
+			Application.Run(new Clock());
+		}
+	}
+}
