07444e79a37e71499600898abed3d4c6f91a61ed
AlexanderKiselyov <a_kiselyov-snz@mail.ru>
2016-03-28 22:39:57 +0300

    1_1

---
+++ контрольная работа_2_1/контрольная_работа_2_1/NewQueueElement.cs
@@ -0,0 +1,17 @@
+﻿namespace PriorityQueue
+{
+	public class NewQueueElement
+	{
+		private PriorityQueue numbers = new PriorityQueue();
+
+		public void Enqueue(int value, int priority)
+		{
+			numbers.Enqueue(value, priority);
+		}
+
+		public int Dequeue()
+		{
+			return numbers.Dequeue();
+		}
+	}
+}
---
+++ контрольная работа_2_1/контрольная_работа_2_1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace PriorityQueue
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			NewQueueElement numbers = new NewQueueElement();
+			numbers.Enqueue(10, 5);
+			numbers.Enqueue(8, 4);
+			numbers.Dequeue();
+			numbers.Dequeue();
+			try
+			{
+				numbers.Dequeue();
+			}
+			catch(NullElementsException)
+			{
+				Console.WriteLine("No elements!");
+			}
+		}
+	}
+}
