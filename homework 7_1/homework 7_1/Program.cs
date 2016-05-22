0ab4b696b504dffde4f34d5e1fd68bd0c9e300aa
AlexanderKiselyov <a_kiselyov-snz@mail.ru>
2016-05-22 21:52:04 +0300

    7_1

---
+++ homework 7_1/homework 7_1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace List
+{
+	class Program
+	{
+		static void Main()
+		{
+			var list = new List<int>();
+			list.Push(10);
+			list.Push(18);
+			list.Push(35);
+			list.Push(9);
+			list.DeleteElement(18);
+			list.DeleteElement(5);
+			//list.Show();
+			foreach (var element in list)
+			{
+				Console.WriteLine(element);
+			}
+		}
+	}
+}
